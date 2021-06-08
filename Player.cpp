de9eaa9ff61a1f4d4fb7efd44d316194274057ab
Matheus Vinicius <54945311+Node-23@users.noreply.github.com>
2021-06-07 23:05:43 -0300

    Player started (#1)
    * Player started
    
    * Player movement and quit game functions added
    
    * Move player with keys 'a' and 'd'
    
    * Now move player has a function to left and right to clean the code

---
+++ Player.cpp
@@ -0,0 +1,47 @@
+#include <GL/freeglut.h>
+
+class Player
+{
+private:
+    float x1, x2, x3, x4;
+    float y1, y2, y3, y4;
+    float moveX;
+
+public:
+    void start()
+    {
+        //If you change the value of X, dont forget to change it on the movePlayer function
+        x1 = 15;
+        y1 = 3;
+        x2 = 15;
+        y2 = -3;
+        x3 = -15;
+        y3 = -3;
+        x4 = -15;
+        y4 = 3;
+    }
+
+    void setPlayer()
+    {
+        glPushMatrix();
+        glTranslatef(0, -80, 0);
+        glColor3f(0, 0, 1);
+        glBegin(GL_QUADS);
+        glVertex2f(x1 + moveX, y1);
+        glVertex2f(x2 + moveX, y2);
+        glVertex2f(x3 + moveX, y3);
+        glVertex2f(x4 + moveX, y4);
+        glEnd();
+        glPopMatrix();
+    }
+
+    void setMoveX(float NewMoveX)
+    {
+        moveX += NewMoveX;
+    }
+
+    float getMoveX()
+    {
+        return moveX;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -2,26 +2,91 @@
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/freeglut.h>
+#include <iostream>
+#include "Player.cpp"
 
+Player player;
 float R, G, B, A;
 
-void init(void){
-	glClearColor(1.0,1.0,1.0,1.0);
-	R=255.0;
-	G=255.0;
-	B=255.0;
-    A=255.0;
+void init(void)
+{
+	glClearColor(255, 255, 255, 255);
+	player.start();
 }
 
 void draw(void);
+void movePlayer(int key, int x, int y);
+void getKeyboard(unsigned char key, int x, int y);
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowSize(512,512);
-	glutInitWindowPosition(100,100);
+	glutInitWindowSize(512, 512);
+	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Game-Project");
 	init();
+	glutDisplayFunc(draw);
+	glutSpecialFunc(movePlayer);
+	glutKeyboardFunc(getKeyboard);
 	glutMainLoop();
 	return 0;
+}
+
+void draw()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-100, 100, -100, 100, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	player.setPlayer();
+	glutSwapBuffers();
+}
+
+void moveLeft()
+{
+	if (player.getMoveX() > -85)
+	{
+		player.setMoveX(-5);
+	}
+}
+
+void moveRight()
+{
+	if (player.getMoveX() < 85)
+	{
+		player.setMoveX(5);
+	}
+}
+
+void movePlayer(int key, int x, int y)
+{
+	if (key == GLUT_KEY_LEFT)
+	{
+		moveLeft();
+	}
+	if (key == GLUT_KEY_RIGHT)
+	{
+		moveRight();
+	}
+	glutPostRedisplay();
+}
+
+void getKeyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+		exit(0);
+		break;
+	case 'a':
+		moveLeft();
+		break;
+	case 'd':
+		moveRight();
+		break;
+	}
+	glutPostRedisplay();
 }
