fd146b6d6dc4dddafd281362099e636d4a95b6a3
Node-23 <matheusvinicius38@live.com>
2021-06-07 22:52:11 -0300

    Player movement and quit game functions added

--- main.cpp
+++ main.cpp
@@ -7,32 +7,69 @@
 Player player;
 float R, G, B, A;
 
-void init(void){
-	glClearColor(255,255,255,255);
+void init(void)
+{
+	glClearColor(255, 255, 255, 255);
 	player.start();
 }
 
 void draw(void);
+void movePlayer(int key, int x, int y);
+void getKeyboard(unsigned char key, int x, int y);
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowSize(512,512);
-	glutInitWindowPosition(100,100);
+	glutInitWindowSize(512, 512);
+	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Game-Project");
 	init();
 	glutDisplayFunc(draw);
+	glutSpecialFunc(movePlayer);
+	glutKeyboardFunc(getKeyboard);
 	glutMainLoop();
 	return 0;
 }
 
-void draw(){
+void draw()
+{
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-100,100,-100,100,-1,1);
+	glOrtho(-100, 100, -100, 100, -1, 1);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	player.setPlayer();
 	glutSwapBuffers();
+}
+
+void movePlayer(int key, int x, int y)
+{
+	if (key == GLUT_KEY_LEFT)
+	{
+		if (player.getMoveX() > -85)
+		{
+			player.setMoveX(-5);
+		}
+	}
+	if (key == GLUT_KEY_RIGHT)
+	{
+		if (player.getMoveX() < 85)
+		{
+			player.setMoveX(5);
+		}
+	}
+	glutPostRedisplay();
+}
+
+void getKeyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+		exit(0);
+		break;
+	}
+	glutPostRedisplay();
 }
